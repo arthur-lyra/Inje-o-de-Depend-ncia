daa1ea45a3e961187f7b7df9e4a40b6144442625
Arthur Lyra Miranda <82905393+arthur-lyra@users.noreply.github.com>
2024-03-25 11:38:37 -0300

    Update ClientTest.java
    Update do cliente

--- Atividade Injeção/src/no/dependency/injection/ClientTest.java
+++ Atividade Injeção/src/no/dependency/injection/ClientTest.java
@@ -3,16 +3,20 @@ package no.dependency.injection;
 public class ClientTest {
 	public static void main(String[] args) {
 
+		String mensagem = "Ola Alex! Mostra um exemplo de DI.";
+		String email = "alex@ifpb.edu.br";
+		String telefone = "83988889999";
+
 		MyApplication app = new MyApplication();
 
 		// Configurando EmailService
 		MessageService emailService = new EmailService();
 		app.setMessageService(emailService);
-		app.processMessages("Ola Alex! Mostra um exemplo de DI.", "alex@ifpb.edu.br");
+		app.processMessages(mensagem, email);
 
 		// Configurando SMSservice
 		MessageService smsService = new SMSservice();
 		app.setMessageService(smsService);
-		app.processMessages("Ola Alex! Mostra um exemplo de DI.", "alex@ifpb.edu.br");
+		app.processMessages(mensagem, telefone);
 	}
 }
